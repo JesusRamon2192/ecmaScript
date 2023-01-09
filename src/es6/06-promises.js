64dff6af736cbe70ba38524988de74b3b1e2c847
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-09 14:07:39 -0600

    Promesas

--- src/es6/06-promises.js
+++ src/es6/06-promises.js
@@ -11,3 +11,32 @@ const anotherFunction = () => {
 anotherFunction()
     .then(response => console.log(response))
     .catch(err => console.log(err));
+
+const datos = [{
+    id:1, 
+    title: 'Iron Man',
+    year: 2008
+},{
+    id:2,
+    title: 'Spiderman',
+    year: 2017
+}, {
+    id:3,
+    title: 'Avengers',
+    year: 2019
+}];
+
+/* const getDatos = () => {
+    return datos;
+} */
+//console.log(getDatos());
+
+const getDatos = () => {
+    return new Promise((resolve, reject) => {
+       setTimeout(()=> {
+        resolve(datos);
+       }, 1500); 
+    })
+}
+
+getDatos().then((datos)=> console.log(datos));
