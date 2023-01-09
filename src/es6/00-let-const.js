53e5be8803a6d9821e7c6fe66904d5a2b3c21b12
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-09 11:25:12 -0600

    Se agregan clases let, const, arrow function y strings

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,22 @@
+var lastName = 'Jesus';
+lastName = 'Oscar';
+console.log(lastName);
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+const animal = 'dog';
+animal = 'cat';
+console.log(animal);
+
+const fruits = () => {
+    if(true){
+        var fruit1 = 'Apple'; // function scope
+        let fruit2 = 'Kiwi'; // block scope
+        const fruit3 = 'Banana'; // block scope
+    }
+    console.log(fruit1, fruit2, fruit3);
+}
+
+fruit();
