58921cb4715557bd124cedabd6d2b081567f46dd
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-10 14:05:25 -0600

    ES9... Expresiones regulares ejemplos

--- src/es9/01-spread.js
+++ src/es9/01-spread.js
@@ -6,3 +6,16 @@ const user = {
 const { username, ...values} = user;
 console.log(username);
 console.log(values);
+
+const objetoOriginal = {a: 1, b: 2}
+const objetoReferencia = objetoOriginal
+const objetoCopia = {...objetoOriginal}
+
+console.log(objetoOriginal);
+console.log(objetoReferencia);
+console.log(objetoCopia);
+
+const original = { datos: [1, [2, 3], 4, 5] }
+const copia = { ...original }
+
+console.log(original, copia);
