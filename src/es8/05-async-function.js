fe4f9ba699e2150ae3225083f1a59f68e44a65e8
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-10 12:30:11 -0600

    Funciones asincronas ejemplos

--- src/es8/05-async-function.js
+++ src/es8/05-async-function.js
@@ -15,3 +15,50 @@ const anotherFunction = async () => {
 console.log('Before');
 anotherFunction();
 console.log('After');
+
+
+// Ejemplo 2
+const datos = [{
+    id:1, 
+    title: 'Iron Man',
+    year: 2008
+},{
+    id:2,
+    title: 'Spiderman',
+    year: 2017
+}, {
+    id:3,
+    title: 'Avengers',
+    year: 2019
+}];
+
+const getDatos = () => {
+    //return datos;
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout (()=> resolve(console.log(datos)), 2000)
+            : reject(new Error('Error de datos'));
+    });
+}
+
+/* getDatos()
+    .then((datos) => {
+        console.log(datos)
+    })
+ */
+
+/* const fnDatosAsync = async() => {
+    const awaitDatos = await getDatos();
+    console.log(awaitDatos);
+} */
+
+async function fnDatosAsync(){
+    try {
+        const awaitDatos = await getDatos();
+        console.log(awaitDatos);
+    } catch (err){
+        console.log(err.message);
+    }
+}
+
+fnDatosAsync()
