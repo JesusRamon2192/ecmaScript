2801855a17852c1074e42c511e457c1ef93a3bc4
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-10 11:52:16 -0600

    Funciones asincronas

--- src/es8/05-async-function.js
+++ src/es8/05-async-function.js
@@ -5,7 +5,7 @@ const fnAsync = () => {
             : reject(new Error('Error!'));
     });
 }
-
+//“async-await” es una manera de trabajar de una forma mas cómoda con promesas, la palabra reservada “async” quiere decir que una función siempre devolverá una promesa. Ahora la palabra reservada “await”, solamente existe dentro de una función “async”, que hace que JavaScript espere hasta que la función promesa responda para continuar con el código dentro de ésta función, pero sin pausar la ejecución del siguiente código.
 const anotherFunction = async () => {
     const something = await fnAsync();
     console.log(something);
