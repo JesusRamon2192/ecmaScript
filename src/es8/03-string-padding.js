59c3cfe0182671f2958fc855b71fad04c4696e27
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-10 11:12:04 -0600

    String paddings y trailing commas

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,5 @@
+const string = 'Hello';
+//Agrega un padding o un espacio antes del string, donde busca rellenar con el segundo parametro los espacios disponibles
+console.log(string.padStart(7, 'Hi'));
+//Agrega el espacio despues del string
+console.log(string.padEnd(6, '_'));
