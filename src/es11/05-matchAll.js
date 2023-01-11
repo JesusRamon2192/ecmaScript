dd9fec1413ae64f8080e3ff33dc4436fcf92ca8f
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-11 13:12:04 -0600

    Globalthis y matchAll

--- src/es11/05-matchAll.js
+++ src/es11/05-matchAll.js
@@ -1,3 +1,5 @@
+//MatchAll sirve para buscar todas las COINCIDENCIAS de una expresión regular enuna cadena de texto y devuelve un iterador con los resultados. La respuesta contiene las dos coincidencias, el incide donde se encuentra yelinput original.
+
 const regex = /\b(Apple)+\b/g;
 
 const fruit = 'Apple, Banana, Kiwi, Apple, Orange, Platano, Banana, Apple';
